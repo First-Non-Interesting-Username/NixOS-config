c9527164a113c571f652135f6ded56fd22f14c42
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-05 20:24:44 +0100

    VScoded again

--- modules/git.nix
+++ modules/git.nix
@@ -2,58 +2,53 @@
   self,
   inputs,
   ...
-}:
-{
+}: {
   flake = {
-    nixosModules.git =
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      {
-        programs.git.enable = lib.mkForce false;
+    nixosModules.git = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      programs.git.enable = lib.mkForce false;
 
-        environment.systemPackages = with pkgs; [
-          git
-          gh
-        ];
-      };
-    homeModules.git =
-      {
-        pkgs,
-        lib,
-        config,
-        gitName,
-        gitEmail,
-        ...
-      }:
-      {
-        programs = {
-          git = {
-            enable = true;
-            settings = {
-              user.name = gitName;
-              user.email = gitEmail;
-              init.defaultBranch = "main";
-              pull.rebase = true;
-            };
-          };
-          gh = {
-            enable = true;
-            settings = {
-              git_protocol = "ssh";
-              prompt = "enabled";
-            };
+      environment.systemPackages = with pkgs; [
+        git
+        gh
+      ];
+    };
+    homeModules.git = {
+      pkgs,
+      lib,
+      config,
+      gitName,
+      gitEmail,
+      ...
+    }: {
+      programs = {
+        git = {
+          enable = true;
+          settings = {
+            user.name = gitName;
+            user.email = gitEmail;
+            init.defaultBranch = "main";
+            pull.rebase = true;
           };
         };
-        sops.secrets.github_pat = { };
-
-        home.sessionVariables = {
-          GH_TOKEN = config.sops.secrets.github_pat.path;
-          NIX_CONFIG = "access-tokens = github.com=${config.sops.secrets.github_pat.path}";
+        gh = {
+          enable = true;
+          settings = {
+            git_protocol = "ssh";
+            prompt = "enabled";
+          };
         };
       };
+      sops.secrets.github_pat = {};
+
+      home.sessionVariables = {
+        GH_TOKEN = config.sops.secrets.github_pat.path;
+        NIX_CONFIG = "access-tokens = github.com=${config.sops.secrets.github_pat.path}";
+      };
+    };
   };
 }
--- modules/secrets.nix
+++ modules/secrets.nix
@@ -2,73 +2,66 @@
   self,
   inputs,
   ...
-}:
-{
+}: {
   flake = {
-    nixosModules.secrets =
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      {
-        environment.systemPackages = with pkgs; [
-          sops
-          age
-          ssh-to-age
-        ];
+    nixosModules.secrets = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      environment.systemPackages = with pkgs; [
+        sops
+        age
+        ssh-to-age
+      ];
 
-        imports = [
-          inputs.sops-nix.nixosModules.sops
-        ];
+      imports = [
+        inputs.sops-nix.nixosModules.sops
+      ];
 
-        sops = {
-          defaultSopsFile = "${self}/secrets/secrets.yaml";
-          age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        };
+      sops = {
+        defaultSopsFile = "${self}/secrets/secrets.yaml";
+        age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
       };
+    };
 
-    nixosModules.secrets-impermanence =
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      {
-        environment.systemPackages = with pkgs; [
-          sops
-          age
-          ssh-to-age
-        ];
+    nixosModules.secrets-impermanence = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      environment.systemPackages = with pkgs; [
+        sops
+        age
+        ssh-to-age
+      ];
 
-        imports = [
-          inputs.sops-nix.nixosModules.sops
-        ];
+      imports = [
+        inputs.sops-nix.nixosModules.sops
+      ];
 
-        sops = {
-          defaultSopsFile = "${self}/secrets/secrets.yaml";
-          age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
-        };
+      sops = {
+        defaultSopsFile = "${self}/secrets/secrets.yaml";
+        age.sshKeyPaths = ["/persist/etc/ssh/ssh_host_ed25519_key"];
       };
+    };
 
-    homeModules.secrets =
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      {
-        imports = [
-          inputs.sops-nix.homeManagerModules.sops
-        ];
+    homeModules.secrets = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      imports = [
+        inputs.sops-nix.homeManagerModules.sops
+      ];
 
-        sops = {
-          defaultSopsFile = "${self}/secrets/secrets.yaml";
-          age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        };
+      sops = {
+        defaultSopsFile = "${self}/secrets/secrets.yaml";
+        age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
       };
+    };
   };
 }
