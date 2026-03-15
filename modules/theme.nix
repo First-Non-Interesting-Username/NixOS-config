61ff331fb42ef9a244af6deeeba2d1b4dc6d3a94
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-15 10:52:34 +0100

    fix: Fixed blurry wallpaper

--- modules/theme.nix
+++ modules/theme.nix
@@ -17,9 +17,10 @@
       colors = config.lib.stylix.colors;
 
       svgContent = ''
-        <svg height="934.526" viewBox="0 0 439.58 247.26" width="439.58mm" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 0h439.58v247.26H0z" fill="#${colors.base00}"/>
-          <g fill-rule="evenodd">
+        <svg height="2160" viewBox="0 0 3840 2160" width="3840"
+             xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h3840v2160H0z" fill="#${colors.base00}"/>
+          <g fill-rule="evenodd" transform="translate(416.4 267.6) scale(6.578)">
             <path d="m194.168 125.017 32.017 55.46-14.714.138-8.548-14.9-8.608 14.82-7.31-.003-3.745-6.468 12.265-21.09-8.707-15.15z" fill="#${colors.base0C}"/>
             <path d="m205.725 102.173-32.022 55.458-7.476-12.674 8.63-14.852-17.14-.046-3.653-6.332 3.73-6.478 24.396.078 8.769-15.117z" fill="#${colors.base0E}"/>
             <path d="m208.18 146.506 64.04.003-7.238 12.812-17.178-.048 8.53 14.866-3.657 6.33-7.475.008-12.131-21.166-17.474-.036z" fill="#${colors.base0B}"/>
@@ -30,15 +31,16 @@
         </svg>
       '';
 
-      svgFile = pkgs.writeText "wallpaper.svg" svgContent;
-
       image = pkgs.runCommand "converted-wallpaper.png" {} ''
         ${pkgs.imagemagick}/bin/magick \
-          -density 300 \
+          -density 96 \
           ${svgFile} \
-          -resize ${toString width}x${toString height} \
+          -resize ${toString width}x${toString height}! \
+          -flatten \
           $out
       '';
+
+      svgFile = pkgs.writeText "wallpaper.svg" svgContent;
     in {
       imports = [
         inputs.stylix.homeModules.stylix
