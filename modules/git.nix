36923351394fd6432c7f203aeacbeb48602d3c58
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-15 12:53:11 +0100

    chore: corrected github token

--- modules/git.nix
+++ modules/git.nix
@@ -2,56 +2,61 @@
   self,
   inputs,
   ...
-}: {
+}:
+{
   flake = {
-    nixosModules.git = {
-      pkgs,
-      lib,
-      config,
-      username,
-      ...
-    }: {
-      programs.git.enable = lib.mkForce false;
+    nixosModules.git =
+      {
+        pkgs,
+        lib,
+        config,
+        username,
+        ...
+      }:
+      {
+        programs.git.enable = lib.mkForce false;
 
-      environment.systemPackages = with pkgs; [
-        git
-        gh
-      ];
-      sops.secrets.github_pat = {
-        owner = username;
-      };
-    };
-    homeModules.git = {
-      pkgs,
-      lib,
-      config,
-      gitName,
-      gitEmail,
-      osConfig,
-      ...
-    }: {
-      programs = {
-        git = {
-          enable = true;
-          settings = {
-            user.name = gitName;
-            user.email = gitEmail;
-            init.defaultBranch = "main";
-            pull.rebase = true;
-          };
-        };
-        gh = {
-          enable = true;
-          settings = {
-            git_protocol = "ssh";
-            prompt = "enabled";
-          };
+        environment.systemPackages = with pkgs; [
+          git
+          gh
+        ];
+        sops.secrets.github_pat = {
+          owner = username;
         };
       };
+    homeModules.git =
+      {
+        pkgs,
+        lib,
+        config,
+        gitName,
+        gitEmail,
+        osConfig,
+        ...
+      }:
+      {
+        programs = {
+          git = {
+            enable = true;
+            settings = {
+              user.name = gitName;
+              user.email = gitEmail;
+              init.defaultBranch = "main";
+              pull.rebase = true;
+            };
+          };
+          gh = {
+            enable = true;
+            settings = {
+              git_protocol = "ssh";
+              prompt = "enabled";
+            };
+          };
+        };
 
-      home.sessionVariables = {
-        GH_TOKEN = osConfig.sops.secrets.github_pat.path;
+        programs.zsh.initExtra = ''
+          export GH_TOKEN="$(cat ${osConfig.sops.secrets.github_pat.path})"
+        '';
       };
-    };
   };
 }
