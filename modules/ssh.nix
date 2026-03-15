eb3d313bcc3b56c7c752697745e2fc23328414aa
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-15 19:13:39 +0100

    feat: added private user ssh keys for hosts

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -9,12 +9,26 @@
       lib,
       config,
       username,
+      hostname,
       ...
     }: {
       programs.ssh.startAgent = true;
       systemd.tmpfiles.rules = [
         "d /home/${username}/.ssh 0700 ${username} users - -"
       ];
+      users.users.${username} = {
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFb1ByQK+SH7b7ZD+Epe5zYDyOUp2V0Sr/GcAfKy8J4y Laptop_key"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhyyqVG8KdfHL00jBin/8rJzaD1Str3lO7N+IeF8fPI Server_key"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGzRUdlC8OdgeZhL9Kn+57GHAmMpkfBG3iqPl3dRYTM Desktop_key"
+        ];
+      };
+      sops.secrets."private_ssh_key/${hostname}" = {
+        owner = username;
+        group = config.users.users.${username}.group;
+        mode = "0600";
+        path = "${config.users.users.${username}.home}/.ssh/id_ed25519";
+      };
     };
     homeModules.ssh = {
       pkgs,
