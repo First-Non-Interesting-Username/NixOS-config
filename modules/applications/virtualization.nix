f0f9a83c22cbfe8807e2975b4c54f03885002450
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-21 15:36:43 +0100

    chore: testing again

--- modules/applications/virtualization.nix
+++ modules/applications/virtualization.nix
@@ -15,47 +15,45 @@
         ...
       }:
       {
-        config = lib.mkIf impermanence {
-          environment.persistence."/persist" = {
-            directories = [ "/var/lib/containers" ];
-            users.${username} = {
-              directories = [
-                ".local/share/containers"
-                ".config/containers"
-                ".local/share/distrobox"
-              ];
-            };
-          };
-        };
-
-        virtualisation = {
-          podman = {
-            enable = true;
-            dockerCompat = true;
-            defaultNetwork.settings.dns_enabled = true;
-          };
-        };
-        boot.kernelModules = [
-          "binder_linux"
-          "ashmem_linux"
-        ];
-        home-manager.users.${username} =
-          {
-            pkgs,
-            lib,
-            config,
-            ...
-          }:
-          {
-            home.packages = with pkgs; [
-              distroshelf
-            ];
-            programs = {
-              distrobox = {
-                enable = true;
+        # We use 'config' as the single top-level attribute
+        config = lib.mkMerge [
+          # Part 1: Conditional persistence
+          (lib.mkIf impermanence {
+            environment.persistence."/persist" = {
+              directories = [ "/var/lib/containers" ];
+              users.${username} = {
+                directories = [
+                  ".local/share/containers"
+                  ".config/containers"
+                  ".local/share/distrobox"
+                ];
               };
             };
-          };
+          })
+
+          # Part 2: Your standard configuration (moved inside config)
+          {
+            virtualisation = {
+              podman = {
+                enable = true;
+                dockerCompat = true;
+                defaultNetwork.settings.dns_enabled = true;
+              };
+            };
+
+            boot.kernelModules = [
+              "binder_linux"
+              "ashmem_linux"
+            ];
+
+            home-manager.users.${username} =
+              { pkgs, ... }:
+              {
+                home.packages = [ pkgs.distroshelf ];
+                programs.distrobox.enable = true;
+              };
+          }
+        ];
       };
 
     nixosModules.virtualization-server =
