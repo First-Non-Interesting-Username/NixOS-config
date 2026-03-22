36b6b7dd20a0515b21a6ac5d287d3970c8269004
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-22 09:34:03 +0100

    docs: add CONTRIBUTING.md and CODE_OF_CONDUCT.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# PLEASE DON'T USE, BROKEN RIGHT NOW
+#
+
+> [!NOTE]
+> This is a learning project in first place.
+> Some choices, such as including full [code of conduct](./CODE_OF_CONDUCT.md) or documentation may feel like overkill for this size of a project.
+> Please point out any issues you encounter. I will be more than happy to fix them and learn on my mistakes.
--- devShells/default.nix
+++ devShells/default.nix
@@ -21,6 +21,22 @@
             ];
           })
         ];
+
+        shellHook = ''
+          HOOK_FILE=".git/hooks/pre-commit"
+          if [ ! -f "$HOOK_FILE" ]; then
+            cat > "$HOOK_FILE" << 'HOOK'
+          #!/bin/sh
+          staged=$(git diff --cached --name-only --diff-filter=ACM | grep '\.nix$')
+          if [ -n "$staged" ]; then
+            alejandra --quiet $staged
+            git add -u
+          fi
+          HOOK
+            chmod +x "$HOOK_FILE"
+            echo "Installed alejandra pre-commit hook"
+          fi
+        '';
       };
     };
 }
