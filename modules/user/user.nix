a2dfdfb06128680a23237707d09d37d28dcbb1b3
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-24 21:23:31 +0100

    chore: change the way hosts are imported in preparation to total reorganization of them

--- modules/user/user.nix
+++ modules/user/user.nix
@@ -2,69 +2,64 @@
   self,
   inputs,
   ...
-}:
-{
+}: {
   flake = {
-    nixosModules.user =
-      {
-        pkgs,
-        lib,
-        config,
-        username,
-        hostname,
-        ...
-      }:
-      {
-        users = {
-          mutableUsers = false;
-          groups.${username} = {
-            gid = 1000;
-          };
-          users.${username} = {
-            isNormalUser = true;
-            uid = 1000;
-            group = username;
-            home = "/home/${username}";
-            description = username;
-            extraGroups = [
-              "wheel"
-              "networkmanager"
-              "video"
-              "audio"
-              "render"
-              "gamemode"
-              "input"
-            ];
-            hashedPasswordFile = config.sops.secrets."sudo_password/${hostname}".path;
-            subUidRanges = [
-              {
-                startUid = 100000;
-                count = 65536;
-              }
-            ];
-            subGidRanges = [
-              {
-                startGid = 100000;
-                count = 65536;
-              }
-            ];
-            linger = true;
-          };
+    nixosModules.user = {
+      pkgs,
+      lib,
+      config,
+      username,
+      hostname,
+      ...
+    }: {
+      users = {
+        mutableUsers = false;
+        groups.${username} = {
+          gid = 1000;
         };
-        sops.secrets."sudo_password/${hostname}" = {
-          neededForUsers = true;
+        users.${username} = {
+          isNormalUser = true;
+          uid = 1000;
+          group = username;
+          home = "/home/${username}";
+          description = username;
+          extraGroups = [
+            "wheel"
+            "networkmanager"
+            "video"
+            "audio"
+            "render"
+            "gamemode"
+            "input"
+          ];
+          hashedPasswordFile = config.sops.secrets."sudo_password/${hostname}".path;
+          subUidRanges = [
+            {
+              startUid = 100000;
+              count = 65536;
+            }
+          ];
+          subGidRanges = [
+            {
+              startGid = 100000;
+              count = 65536;
+            }
+          ];
+          linger = true;
         };
       };
-    nixosModules.user-debug =
-      {
-        pkgs,
-        lib,
-        config,
-        username,
-        ...
-      }:
-      {
-        users.users.root.initialPassword = "debug";
+      sops.secrets."sudo_password/${hostname}" = {
+        neededForUsers = true;
       };
+    };
+    nixosModules.user-debug = {
+      pkgs,
+      lib,
+      config,
+      username,
+      ...
+    }: {
+      users.users.root.initialPassword = "debug";
+    };
   };
 }
