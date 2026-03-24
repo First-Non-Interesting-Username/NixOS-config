cf60847f03e16467195b340777c89dba4574d805
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-24 21:20:33 +0100

    chore: change the way hosts are imported in preparation to total reorganization of them

--- hosts/armin/default.nix
+++ hosts/armin/default.nix
@@ -3,17 +3,14 @@
   inputs,
   lib,
   ...
-}:
-let
-  Hostname = "iroh";
+}: let
+  Hostname = "armin";
   Username = "nixi";
   GitName = "First-Non-Interesting-Username";
   GitEmail = "janekmusin@proton.me";
-  Domain = "iameasytoremember.duckdns.org";
-  Width = 2560;
-  Height = 1440;
-in
-{
+  Width = 1920;
+  Height = 1080;
+in {
   flake.nixosConfigurations.${Hostname} = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {
@@ -22,28 +19,40 @@ in
       gitName = GitName;
       gitEmail = GitEmail;
       hostname = Hostname;
-      domain = Domain;
-      impermanence = false;
+      impermanence = true;
     };
     modules = [
       # System modules go here
       self.nixosModules.home-manager
+      self.nixosModules.flatpak
       self.nixosModules.git
-      self.nixosModules.secrets
-      self.nixosModules.ssh-server
-      self.nixosModules.ssh
+      self.nixosModules.secrets-impermanence
+      self.nixosModules.ssh-impermanence
       self.nixosModules.shell
       self.nixosModules.bootloader
-      self.nixosModules.nix
-      self.nixosModules.user
-      self.nixosModules.networking-server
-      self.nixosModules.locale
-      self.nixosModules."hardware-${Hostname}"
-      self.nixosModules.virtualization-server
-      self.nixosModules.nps
       self.nixosModules.update
+      self.nixosModules.nix
+      self.nixosModules.wayland
+      self.nixosModules.input
+      self.nixosModules.user
+      self.nixosModules.virtualization-desktop
+      # self.nixosModules."hardware-${Hostname}"
+      self.nixosModules.networking-desktop
+      self.nixosModules.audio
+      self.nixosModules.printing
+      self.nixosModules.locale
+      self.nixosModules.power
+      self.nixosModules.plasma
+      # self.nixosModules.hyprland
+      self.nixosModules.gaming
+      self.nixosModules.programs-desktop
+      self.nixosModules.kernel-laptop
+      self.nixosModules.terminal
       self.nixosModules.theme
+      self.nixosModules.browser
+      self.nixosModules.IDE
       self.nixosModules.direnv
+      self.nixosModules.moonlight
       inputs.home-manager.nixosModules.home-manager
       {
         home-manager.useGlobalPkgs = true;
@@ -53,10 +62,9 @@ in
           username = Username;
           gitName = GitName;
           gitEmail = GitEmail;
-          hostname = Hostname;
-          domain = Domain;
           width = Width;
           height = Height;
+          hostname = Hostname;
         };
       }
     ];
--- hosts/iroh/default.nix
+++ hosts/iroh/default.nix
@@ -3,16 +3,15 @@
   inputs,
   lib,
   ...
-}:
-let
-  Hostname = "armin";
+}: let
+  Hostname = "iroh";
   Username = "nixi";
   GitName = "First-Non-Interesting-Username";
   GitEmail = "janekmusin@proton.me";
-  Width = 1920;
-  Height = 1080;
-in
-{
+  Domain = "iameasytoremember.duckdns.org";
+  Width = 2560;
+  Height = 1440;
+in {
   flake.nixosConfigurations.${Hostname} = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {
@@ -21,40 +20,28 @@ in
       gitName = GitName;
       gitEmail = GitEmail;
       hostname = Hostname;
-      impermanence = true;
+      domain = Domain;
+      impermanence = false;
     };
     modules = [
       # System modules go here
       self.nixosModules.home-manager
-      self.nixosModules.flatpak
       self.nixosModules.git
-      self.nixosModules.secrets-impermanence
-      self.nixosModules.ssh-impermanence
+      self.nixosModules.secrets
+      self.nixosModules.ssh-server
+      self.nixosModules.ssh
       self.nixosModules.shell
       self.nixosModules.bootloader
-      self.nixosModules.update
       self.nixosModules.nix
-      self.nixosModules.wayland
-      self.nixosModules.input
       self.nixosModules.user
-      self.nixosModules.virtualization-desktop
-      # self.nixosModules."hardware-${Hostname}"
-      self.nixosModules.networking-desktop
-      self.nixosModules.audio
-      self.nixosModules.printing
+      self.nixosModules.networking-server
       self.nixosModules.locale
-      self.nixosModules.power
-      self.nixosModules.plasma
-      # self.nixosModules.hyprland
-      self.nixosModules.gaming
-      self.nixosModules.programs-desktop
-      self.nixosModules.kernel-laptop
-      self.nixosModules.terminal
+      self.nixosModules."hardware-${Hostname}"
+      self.nixosModules.virtualization-server
+      self.nixosModules.nps
+      self.nixosModules.update
       self.nixosModules.theme
-      self.nixosModules.browser
-      self.nixosModules.IDE
       self.nixosModules.direnv
-      self.nixosModules.moonlight
       inputs.home-manager.nixosModules.home-manager
       {
         home-manager.useGlobalPkgs = true;
@@ -64,9 +51,10 @@ in
           username = Username;
           gitName = GitName;
           gitEmail = GitEmail;
+          hostname = Hostname;
+          domain = Domain;
           width = Width;
           height = Height;
-          hostname = Hostname;
         };
       }
     ];
