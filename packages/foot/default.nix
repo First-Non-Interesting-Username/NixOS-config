5bd9389feeac1c7adc3ef9400517673f992c71c7
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-03-30 21:09:41 +0200

    ci: modify flake-build workflow to include packages

--- packages/foot/default.nix
+++ packages/foot/default.nix
@@ -17,7 +17,17 @@
     {
       imports = [ wlib.wrapperModules.foot ];
 
+      extraPackages = [ pkgs.nerd-fonts.jetbrains-mono ];
+
       settings = {
+        main = {
+          include = "${pkgs.foot.themes}/share/foot/themes/gruvbox-dark";
+
+          font = "JetBrainsMono Nerd Font:size=12";
+        };
+
+        colors = {
+        };
       };
     };
 }
