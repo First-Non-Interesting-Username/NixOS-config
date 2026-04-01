6b1be51a45ed209e9e7c83487d8a5547e6837433
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-04-01 11:26:20 +0200

    feat: fix vicinae styling (?)

--- modules/applications/vicinae.nix
+++ modules/applications/vicinae.nix
@@ -1,65 +1,78 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   flake = {
-    nixosModules.vicinae = {
-      lib,
-      username,
-      impermanence,
-      ...
-    }: {
-      imports = lib.optionals impermanence [
-        {
-          environment.persistence."/persist" = {
-            users.${username} = {
-              directories = [
-                ".local/share/vicinae"
-                ".config/vicinae"
-              ];
-            };
-          };
-        }
-      ];
-
-      home-manager.users.${username} = {
-        pkgs,
+    nixosModules.vicinae =
+      {
         lib,
+        username,
+        impermanence,
         ...
-      }: {
-        imports = [
-          inputs.vicinae.homeManagerModules.default
+      }:
+      {
+        imports = lib.optionals impermanence [
+          {
+            environment.persistence."/persist" = {
+              users.${username} = {
+                directories = [
+                  ".local/share/vicinae"
+                  ".config/vicinae"
+                ];
+              };
+            };
+          }
         ];
-        services = {
-          vicinae = {
-            enable = true;
-            systemd = {
-              enable = true;
-              autoStart = true;
-              environment = {
-                USE_LAYER_SHELL = 1;
-              };
-            };
-            settings = {
-              close_on_focus_loss = true;
-              consider_preedit = true;
-              pop_to_root_on_close = true;
-              favicon_service = "twenty";
-              search_files_in_root = true;
-              launcher_window = {
-                opacity = lib.mkForce 0.98;
-              };
-            };
-            extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
-              bluetooth
-              nix
-              power-profile
-              vscode-recents
-              ssh
-              searxng
-              hypr-keybinds
-              it-tools
+
+        home-manager.users.${username} =
+          {
+            pkgs,
+            lib,
+            ...
+          }:
+          {
+            imports = [
+              inputs.vicinae.homeManagerModules.default
             ];
+            services = {
+              vicinae = {
+                enable = true;
+                systemd = {
+                  enable = true;
+                  autoStart = true;
+                  environment = {
+                    USE_LAYER_SHELL = 1;
+                  };
+                };
+                settings = {
+                  close_on_focus_loss = true;
+                  consider_preedit = true;
+                  pop_to_root_on_close = true;
+                  favicon_service = "twenty";
+                  search_files_in_root = true;
+                  launcher_window = {
+                    opacity = lib.mkForce 0.98;
+                  };
+                  theme = {
+                    light = {
+                      name = "stylix";
+                    }; # Fallback if system switches
+                    dark = {
+                      name = "stylix";
+                    }; # This is what you want for dark mode
+                  };
+                };
+                extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
+                  bluetooth
+                  nix
+                  power-profile
+                  vscode-recents
+                  ssh
+                  searxng
+                  hypr-keybinds
+                  it-tools
+                ];
+              };
+            };
           };
-        };
       };
-    };
   };
 }
