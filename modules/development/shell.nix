4e0a9361389f333e796b6a54f5ca4f61367a4f6c
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-04-04 15:33:52 +0200

    feat: add ripgrep

--- modules/development/shell.nix
+++ modules/development/shell.nix
@@ -1,211 +1,217 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   flake = {
-    nixosModules.shell = {
-      pkgs,
-      lib,
-      username,
-      impermanence,
-      ...
-    }: {
-      imports = lib.optional impermanence {
-        environment.persistence."/persist" = {
-          users.${username} = {
-            directories = [
-              ".local/share/atuin"
-              ".local/share/zoxide"
-              ".config/zsh"
-              ".cache/tealdeer"
-              ".local/share/nix-index"
-            ];
-          };
-        };
-      };
-
-      users.users.${username}.shell = pkgs.zsh;
-      programs.zsh.enable = true;
-      home-manager.users.${username} = {
+    nixosModules.shell =
+      {
         pkgs,
-        config,
+        lib,
+        username,
+        impermanence,
         ...
-      }: {
-        imports = [
-          inputs.nix-index-database.homeModules.nix-index
-        ];
-        programs = {
-          zsh = {
-            enable = true;
-            autocd = true;
-            enableCompletion = true;
-            autosuggestion.enable = true;
-            syntaxHighlighting.enable = true;
-            dotDir = "${config.xdg.configHome}/zsh";
-            oh-my-zsh = {
-              enable = true;
-              plugins = [
-                "git"
-                "copyfile"
-                "copypath"
-                "sudo"
+      }:
+      {
+        imports = lib.optional impermanence {
+          environment.persistence."/persist" = {
+            users.${username} = {
+              directories = [
+                ".local/share/atuin"
+                ".local/share/zoxide"
+                ".config/zsh"
+                ".cache/tealdeer"
+                ".local/share/nix-index"
               ];
-              theme = "";
             };
-            initContent = ''
-              fastfetch
-            '';
           };
+        };
 
-          nix-index-database.comma.enable = true;
-          nix-index = {
-            enable = true;
-            package = pkgs.nix-index;
-            enableZshIntegration = true;
-          };
-
-          starship = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-
-          atuin = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-
-          eza = {
-            enable = true;
-            enableZshIntegration = true;
-            icons = "auto";
-            git = true;
-          };
-
-          zoxide = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-
-          tealdeer = {
-            enable = true;
-            enableAutoUpdates = true;
-          };
-
-          television = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-
-          pay-respects = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-
-          lazygit = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-
-          btop.enable = true;
-
-          bat.enable = true;
-
-          fd.enable = true;
-
-          fastfetch = {
-            enable = true;
-            settings = {
-              display = {
-                separator = "  ";
-                color = "blue";
+        users.users.${username}.shell = pkgs.zsh;
+        programs.zsh.enable = true;
+        home-manager.users.${username} =
+          {
+            pkgs,
+            config,
+            ...
+          }:
+          {
+            imports = [
+              inputs.nix-index-database.homeModules.nix-index
+            ];
+            programs = {
+              zsh = {
+                enable = true;
+                autocd = true;
+                enableCompletion = true;
+                autosuggestion.enable = true;
+                syntaxHighlighting.enable = true;
+                dotDir = "${config.xdg.configHome}/zsh";
+                oh-my-zsh = {
+                  enable = true;
+                  plugins = [
+                    "git"
+                    "copyfile"
+                    "copypath"
+                    "sudo"
+                  ];
+                  theme = "";
+                };
+                initContent = ''
+                  fastfetch
+                '';
               };
-              modules = [
-                {
-                  type = "title";
-                  key = "";
-                  color = {
-                    user = "blue";
-                    at = "white";
-                    host = "blue";
+
+              nix-index-database.comma.enable = true;
+              nix-index = {
+                enable = true;
+                package = pkgs.nix-index;
+                enableZshIntegration = true;
+              };
+
+              starship = {
+                enable = true;
+                enableZshIntegration = true;
+              };
+
+              atuin = {
+                enable = true;
+                enableZshIntegration = true;
+              };
+
+              eza = {
+                enable = true;
+                enableZshIntegration = true;
+                icons = "auto";
+                git = true;
+              };
+
+              zoxide = {
+                enable = true;
+                enableZshIntegration = true;
+              };
+
+              tealdeer = {
+                enable = true;
+                enableAutoUpdates = true;
+              };
+
+              television = {
+                enable = true;
+                enableZshIntegration = true;
+              };
+
+              pay-respects = {
+                enable = true;
+                enableZshIntegration = true;
+              };
+
+              lazygit = {
+                enable = true;
+                enableZshIntegration = true;
+              };
+
+              btop.enable = true;
+
+              bat.enable = true;
+
+              fd.enable = true;
+
+              fastfetch = {
+                enable = true;
+                settings = {
+                  display = {
+                    separator = "  ";
+                    color = "blue";
                   };
-                }
-                {
-                  type = "os";
-                  key = "󱄅";
-                }
-                {
-                  type = "kernel";
-                  key = "";
-                }
-                {
-                  type = "uptime";
-                  key = "󰅐";
-                }
-                "break"
-                {
-                  type = "board";
-                  key = "󱩊";
-                }
-                {
-                  type = "cpu";
-                  key = "";
-                }
-                {
-                  type = "gpu";
-                  key = "󰢮";
-                }
-                {
-                  type = "memory";
-                  key = "";
-                  format = "{1} / {2}";
-                }
-                {
-                  type = "disk";
-                  key = "󰋊";
-                  format = "{1} / {2} ({9})";
-                }
-                {
-                  type = "display";
-                  key = "󰍹";
-                }
-                "break"
-                {
-                  type = "de";
-                  key = "󰧨";
-                }
-                {
-                  type = "wm";
-                  key = "";
-                }
-                {
-                  type = "shell";
-                  key = "";
-                }
-                {
-                  type = "terminal";
-                  key = "";
-                }
-                {
-                  type = "packages";
-                  key = "󰏖";
-                }
-              ];
+                  modules = [
+                    {
+                      type = "title";
+                      key = "";
+                      color = {
+                        user = "blue";
+                        at = "white";
+                        host = "blue";
+                      };
+                    }
+                    {
+                      type = "os";
+                      key = "󱄅";
+                    }
+                    {
+                      type = "kernel";
+                      key = "";
+                    }
+                    {
+                      type = "uptime";
+                      key = "󰅐";
+                    }
+                    "break"
+                    {
+                      type = "board";
+                      key = "󱩊";
+                    }
+                    {
+                      type = "cpu";
+                      key = "";
+                    }
+                    {
+                      type = "gpu";
+                      key = "󰢮";
+                    }
+                    {
+                      type = "memory";
+                      key = "";
+                      format = "{1} / {2}";
+                    }
+                    {
+                      type = "disk";
+                      key = "󰋊";
+                      format = "{1} / {2} ({9})";
+                    }
+                    {
+                      type = "display";
+                      key = "󰍹";
+                    }
+                    "break"
+                    {
+                      type = "de";
+                      key = "󰧨";
+                    }
+                    {
+                      type = "wm";
+                      key = "";
+                    }
+                    {
+                      type = "shell";
+                      key = "";
+                    }
+                    {
+                      type = "terminal";
+                      key = "";
+                    }
+                    {
+                      type = "packages";
+                      key = "󰏖";
+                    }
+                  ];
+                };
+              };
+            };
+
+            home.packages = with pkgs; [
+              trash-cli
+              ugrep
+              ripgrep
+            ];
+
+            home.shellAliases = {
+              cat = "bat --style=plain --pager=never";
+              igrep = "ug -t";
+              rm = "trash-put";
+              tp = "trash-put";
+              tl = "trash-list";
+              te = "trash-empty";
+              bin = "nc termbin.com 9999";
             };
           };
-        };
-
-        home.packages = with pkgs; [
-          trash-cli
-          ugrep
-        ];
-
-        home.shellAliases = {
-          cat = "bat --style=plain --pager=never";
-          igrep = "ug -t";
-          rm = "trash-put";
-          tp = "trash-put";
-          tl = "trash-list";
-          te = "trash-empty";
-          bin = "nc termbin.com 9999";
-        };
       };
-    };
   };
 }
