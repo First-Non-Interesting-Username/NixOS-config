c644925278c50d8757c9aaee9ce088d4dfd4e15a
First-Non-Interesting-Username <196055729+First-Non-Interesting-Username@users.noreply.github.com>
2026-04-04 11:50:58 +0000

    style: nix maintenance (statix, deadnix, alejandra)

--- modules/iso/default.nix
+++ modules/iso/default.nix
@@ -1,44 +1,37 @@
-{ self, ... }:
-{
+{self, ...}: {
   flake = {
-    nixosModules.iso =
-      {
+    nixosModules.iso = {
+      username,
+      pkgs,
+      ...
+    }: {
+      environment.defaultPackages = with pkgs; [
+        nano
+        netcat
+        disko
+        nixos-anywhere
+        micro
+      ];
+
+      isoImage.contents = [
+        {
+          source = self.outPath;
+          target = "/flake-source";
+        }
+      ];
+
+      systemd.tmpfiles.rules = [
+        "C /etc/nixos - - - - /flake-source"
+        "Z /etc/nixos 0777 ${username} ${username} -"
+      ];
+
+      home-manager.users.${username} = {
+        osConfig,
         lib,
-        username,
-        impermanence,
-        pkgs,
         ...
-      }:
-      {
-        environment.defaultPackages = with pkgs; [
-          nano
-          netcat
-          disko
-          nixos-anywhere
-          micro
-        ];
-
-        isoImage.contents = [
-          {
-            source = self.outPath;
-            target = "/flake-source";
-          }
-        ];
-
-        systemd.tmpfiles.rules = [
-          "C /etc/nixos - - - - /flake-source"
-          "Z /etc/nixos 0777 ${username} ${username} -"
-        ];
-
-        home-manager.users.${username} =
-          {
-            osConfig,
-            lib,
-            ...
-          }:
-          {
-            home.stateVersion = lib.mkForce osConfig.system.stateVersion;
-          };
+      }: {
+        home.stateVersion = lib.mkForce osConfig.system.stateVersion;
       };
+    };
   };
 }
