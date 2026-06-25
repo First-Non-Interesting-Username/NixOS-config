78676fa9aff3f9e4c384d76b9362ee5a9df83e80
First-Non-Interesting-Username <janekmusin@proton.me>
2026-06-25 18:04:54 +0200

    refactor: include gaming module in armin

--- hosts/armin/default.nix
+++ hosts/armin/default.nix
@@ -19,6 +19,7 @@ in {
       self.nixosModules.browser
       self.nixosModules.direnv
       self.nixosModules.flatpak
+      self.nixosModules.gaming
       self.nixosModules.git
       self.nixosModules.GNOME
       self.nixosModules.home-manager
