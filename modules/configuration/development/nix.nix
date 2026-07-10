c1858fd3c15238396493ed2c0d4504dc1d2c30f1
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-07-10 12:36:28 +0200

    ci: don't build the cachyos kernel

--- modules/configuration/development/nix.nix
+++ modules/configuration/development/nix.nix
@@ -25,7 +25,6 @@ _: {
           substituters = [
             "https://cache.nixos.org/"
             "https://nix-community.cachix.org/"
-            "https://vicinae.cachix.org"
             "https://matrix.cachix.org"
             "https://devenv.cachix.org"
           ];
@@ -33,7 +32,6 @@ _: {
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
             "matrix.cachix.org-1:uZWavEIj0/oIRHPjh+OG586y4nXBlyI0xkYfZBfDx7w="
             "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           ];
