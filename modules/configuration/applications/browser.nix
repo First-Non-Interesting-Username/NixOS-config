9fe195fb8dcc27349470b6b3db9cf2885802888b
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-17 17:32:34 +0200

    feat: don't open a new tab with startpage

--- modules/configuration/applications/browser.nix
+++ modules/configuration/applications/browser.nix
@@ -100,6 +100,10 @@
                             name = "query";
                             value = "{searchTerms}";
                           }
+                          {
+                            name = "prfe";
+                            value = "43de26394a14001889a11b904e373f83b4833751e6bb6aaf1cb898ea811899be8de2ce9e000306e7e258ecb3a1c9eddb6705f30f6aa6441baa704f36c011fc8bfe68d3114b885135b412f73b4b16eaae";
+                          }
                         ];
                       }
                     ];
