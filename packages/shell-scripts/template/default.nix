b745bb79226a142bd5772805089c2bb2c4306db8
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-13 22:16:11 +0200

    docs: create an example shell script

---
+++ packages/shell-scripts/template/default.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2026 First-Non-Interesting-Username
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+_: {
+  perSystem = {pkgs, ...}: {
+    packages.CHANGEME = pkgs.writeShellApplication {
+      name = "CHANGEME";
+      runtimeInputs = with pkgs; [
+      ];
+      text = ''
+        # here is the place for the script
+      '';
+    };
+  };
+}
