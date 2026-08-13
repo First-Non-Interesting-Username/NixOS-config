77b421049236b95156be7e38227f9cb8ec0b2a03
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-13 22:40:06 +0200

    refactor: reorganize modules a little

--- packages/shell-scripts/template/default.nix
+++ packages/shell-scripts/template/default.nix
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 _: {
   perSystem = {pkgs, ...}: {
-    packages.CHANGEME = pkgs.writeShellApplication {
+    packages.shell-scripts-CHANGEME = pkgs.writeShellApplication {
       name = "CHANGEME";
       runtimeInputs = with pkgs; [
       ];
