e13d1707e569db850bc9b47091899bc40685e624
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-16 20:53:40 +0200

    tests: add gnome-minimal test

---
+++ checks/graphical/gnome-minimal.nix
@@ -0,0 +1,52 @@
+{self, ...}: {
+  perSystem = {pkgs, ...}: let
+    checkname = "gnome-minimal";
+  in {
+    checks.${checkname} = pkgs.testers.runNixOSTest {
+      name = checkname;
+
+      nodes.default = {
+        lib,
+        pkgs,
+        ...
+      }: {
+        imports = [
+          self.nixosModules.DE
+          self.nixosModules.user
+          self.nixosModules.stylix
+          self.nixosModules.preservation
+        ];
+        custom = {
+          user = {
+            enable = true;
+            name = "test";
+            password = "test";
+          };
+          stylix = {
+            enable = true;
+            image = {
+              width = lib.mkDefault "2560";
+              height = lib.mkDefault "1440";
+              enable = true;
+            };
+            base16Scheme = "gruvbox-dark";
+            icons = {
+              package = pkgs.morewaita-icon-theme;
+              name = "MoreWaita";
+            };
+          };
+          preservation.enable = false;
+          DE = {
+            enable = true;
+            name = "gnome";
+          };
+        };
+      };
+
+      testScript = ''
+        machine.wait_for_unit("graphical.target")
+        machine.wait_for_unit("gdm.service")
+      '';
+    };
+  };
+}
