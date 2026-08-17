3e43a099892d125ddc8346580c676d52461222c5
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-17 20:15:28 +0200

    tests: do not test the gaming module

--- checks/modules/configuration/applications/programs.nix
+++ checks/modules/configuration/applications/programs.nix
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 {self, ...}: {
   perSystem = {pkgs, ...}: let
-    checkname = "gaming";
+    checkname = "programs";
     username = "test";
   in {
     checks.${checkname} = pkgs.testers.runNixOSTest {
@@ -14,7 +14,7 @@
           self.nixosModules.user
           self.nixosModules.preservation
           self.nixosModules.home-manager
-          self.nixosModules.gaming
+          self.nixosModules.programs
         ];
         custom = {
           user = {
