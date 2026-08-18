d137cc02b0a90b8865ae6a400235447f9a041c18
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-18 17:49:27 +0200

    tests: add virtualization check

---
+++ checks/modules/configuration/services/virtualization.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2026 First-Non-Interesting-Username
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+{self, ...}: {
+  perSystem = {pkgs, ...}: let
+    checkname = "virtualization";
+    username = "test";
+  in {
+    checks.${checkname} = pkgs.testers.runNixOSTest {
+      name = checkname;
+
+      nodes.machine = {...}: {
+        imports = [
+          self.nixosModules.user
+          self.nixosModules.preservation
+          self.nixosModules.home-manager
+          self.nixosModules.virtualization-desktop
+        ];
+        custom = {
+          user = {
+            enable = true;
+            name = username;
+            password = username;
+          };
+        };
+      };
+
+      testScript = ''
+        machine.succeed("lima --version")
+        machine.succeed("distrobox --version")
+      '';
+    };
+  };
+}
