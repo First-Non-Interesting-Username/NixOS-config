d836d6f2a4fd4101f31cb5a1d8cf47d7ac101be1
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-19 21:01:38 +0200

    feat: add mend

--- modules/configuration/development/git.nix
+++ modules/configuration/development/git.nix
@@ -38,7 +38,7 @@ _: {
         config,
         ...
       }: {
-        home.packages = with pkgs; [onefetch];
+        home.packages = with pkgs; [onefetch meld];
         programs = {
           git = {
             enable = true;
@@ -52,6 +52,10 @@ _: {
               };
               init.defaultBranch = "main";
               pull.rebase = true;
+              rerere.enabled = true;
+              merge.tool = "meld";
+              mergetool.keepBackup = false;
+              mergetool.trustExitCode = true;
             };
           };
           gh = {
@@ -96,7 +100,7 @@ _: {
       ];
 
       home-manager.users.${config.custom.user.name} = {pkgs, ...}: {
-        home.packages = with pkgs; [onefetch];
+        home.packages = with pkgs; [onefetch meld];
         programs = {
           git = {
             enable = true;
@@ -110,6 +114,10 @@ _: {
               };
               init.defaultBranch = "main";
               pull.rebase = true;
+              rerere.enabled = true;
+              merge.tool = "meld";
+              mergetool.keepBackup = false;
+              mergetool.trustExitCode = true;
             };
           };
 
