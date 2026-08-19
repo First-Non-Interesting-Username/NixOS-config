231f76d441ba59d29f028be6d51419876ac07f36
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-19 19:40:41 +0200

    merge: wahatever

--- checks/_example-checks/basic.nix
+++ checks/_example-checks/basic.nix
@@ -1,9 +1,6 @@
-<<<<<<< HEAD:checks/_example-checks/basic.nix
 # SPDX-FileCopyrightText: 2026 First-Non-Interesting-Username
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-=======
->>>>>>> 952a5bc (style: nix maintenance (statix, deadnix, alejandra)):checks/_template.nix
 _: {
   perSystem = {pkgs, ...}: let
     checkname = "CHANGEME";
@@ -11,14 +8,10 @@ _: {
     checks.${checkname} = pkgs.testers.runNixOSTest {
       name = checkname;
 
-<<<<<<< HEAD:checks/_example-checks/basic.nix
       # Gh actions aarch64 runners don't have qemu
       requiredFeatures.kvm = pkgs.stdenv.hostPlatform.isx86_64;
 
       nodes.machine = _: {
-=======
-      nodes.default = _: {
->>>>>>> 952a5bc (style: nix maintenance (statix, deadnix, alejandra)):checks/_template.nix
         # VM config goes here
       };
 
