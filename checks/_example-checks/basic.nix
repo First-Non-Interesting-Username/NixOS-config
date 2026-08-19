121a2bc57e9ec4ec5bf7ebc9b3d1fc6c7a75fc6a
First-Non-Interesting-Username <196055729+First-Non-Interesting-Username@users.noreply.github.com>
2026-08-19 06:40:13 +0000

    style: nix maintenance (statix, deadnix, alejandra)

--- checks/_example-checks/basic.nix
+++ checks/_example-checks/basic.nix
@@ -1,6 +1,9 @@
+<<<<<<< HEAD:checks/_example-checks/basic.nix
 # SPDX-FileCopyrightText: 2026 First-Non-Interesting-Username
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+=======
+>>>>>>> 952a5bc (style: nix maintenance (statix, deadnix, alejandra)):checks/_template.nix
 _: {
   perSystem = {pkgs, ...}: let
     checkname = "CHANGEME";
@@ -8,10 +11,14 @@ _: {
     checks.${checkname} = pkgs.testers.runNixOSTest {
       name = checkname;
 
+<<<<<<< HEAD:checks/_example-checks/basic.nix
       # Gh actions aarch64 runners don't have qemu
       requiredFeatures.kvm = pkgs.stdenv.hostPlatform.isx86_64;
 
       nodes.machine = _: {
+=======
+      nodes.default = _: {
+>>>>>>> 952a5bc (style: nix maintenance (statix, deadnix, alejandra)):checks/_template.nix
         # VM config goes here
       };
 
