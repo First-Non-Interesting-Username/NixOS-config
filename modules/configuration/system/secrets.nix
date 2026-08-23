eacf60af60cc477ff7cab30bb31f8474922bfa1c
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-23 19:45:10 +0200

    fix: use proper path for secrets

--- modules/configuration/system/secrets.nix
+++ modules/configuration/system/secrets.nix
@@ -12,7 +12,12 @@
       lib,
       config,
       ...
-    }: {
+    }: let
+      preservationDir =
+        if config.custom.preservation.enable
+        then "/persist"
+        else "";
+    in {
       imports = [
         inputs.sops-nix.nixosModules.sops
         # TODO: Create secrets module (same concept as preservation module)
@@ -21,7 +26,7 @@
 
       preservation.preserveAt = lib.mkIf config.custom.preservation.enable {
         "/persist" = {
-          directories = ["/var/lib/sops-nix"];
+          directories = ["${preservationDir}/var/lib/sops-nix"];
           users.${config.custom.user.name} = {
             directories = [".config/sops/age"];
           };
@@ -38,7 +43,7 @@
         defaultSopsFile = "${self}/secrets/secrets.yaml";
 
         age = {
-          keyFile = "/var/lib/sops-nix/keys.txt";
+          keyFile = "/persist/var/lib/sops-nix/keys.txt";
           generateKey = false;
           sshKeyPaths = [];
         };
