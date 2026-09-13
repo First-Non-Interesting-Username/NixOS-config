609e98d4ac49f4fa00602ec8101344bba5a0c648
First-Non-Interesting-Username <janekmusin@proton.me>
2026-09-13 17:29:48 +0200

    feat: add llama cpp

---
+++ modules/configuration/applications/llama-cpp.nix
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2026 First-Non-Interesting-Username
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+_: {
+  flake = {
+    nixosModules.llama-cpp = {
+      lib,
+      config,
+      pkgs,
+      ...
+    }: {
+      preservation.preserveAt = lib.mkIf config.custom.preservation.enable {
+        "/persist" = {
+          users.${config.custom.user.name} = {
+            directories = [
+              ".cache/llama.cpp"
+              ".cache/huggingface"
+            ];
+          };
+        };
+      };
+
+      home-manager.users.${config.custom.user.name} = _: {
+        home.packages = with pkgs; [
+          llama-cpp-vulkan
+        ];
+      };
+    };
+  };
+}
